1c2bd37b997060f9e848b9c9b31d846f19661c9a
Sammaye <sam.millman@googlemail.com>
2012-07-17 19:08:52 +0100

    Added JQuery

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 	<link href="/css/main.css" rel="stylesheet" type="text/css">
 
+	<script type="text/javascript" src="/js/jquery.js"></script>
 	<script type="text/javascript" src="/js/video_uploader.js"></script>
 	<script type="text/javascript">
 	$(function(){
