f8ce574b00f44e6b863be384520b85f3196ba49d
Sammaye <sam.millman@googlemail.com>
2012-07-17 19:05:46 +0100

    Added the unique identifier to the upload form

--- upload_form.php
+++ upload_form.php
@@ -1,13 +1,22 @@
-<div class="upload_item" data-id='<?php echo $u_id ?>' id="upload_item_<?php echo $u_id ?>">
+<?php
 
-	<div class="uploadForm" id="uploadForm_<?php echo $u_id ?>">
-		<form action="http://<?php echo glue::$params['uploadBase'] ?>/video/upload_to_server?id=<?php echo $u_id ?>" target="u_ifr<?php echo $u_id ?>" method="post" enctype="multipart/form-data">
-			<a href="javascript: void(0)" class='add_upload'>Click here to Upload a Video
-			<input type="hidden" name="UPLOAD_IDENTIFIER" value="<?php echo $u_id ?>" /><input type="file" name="<?php echo $u_id ?>" id="<?php echo $u_id ?>" class='file_upload' /></a>
-		</form><iframe name="u_ifr<?php echo $u_id ?>" style="display:none;" id="u_ifr<?php echo $u_id ?>"></iframe>
+	// This produces a unqiue ID for us each time so we can use this as the file ID
+	$unique_upload_id = md5( uniqid( rand(1,255).rand(45,80).rand(112,350), true ) );
+
+?>
+
+<div class="upload_item" data-id='<?php echo $unique_upload_id ?>' id="upload_item_<?php echo $unique_upload_id ?>">
+
+	<div class="uploadForm" id="uploadForm_<?php echo $unique_upload_id ?>">
+		<form action="http://<?php echo glue::$params['uploadBase'] ?>/video/upload_to_server?id=<?php echo $unique_upload_id ?>"
+			target="u_ifr<?php echo $unique_upload_id ?>" method="post" enctype="multipart/form-data">
+
+			<a href="javascript: void(0)" class='add_upload'>Click here to Upload a Video<input type="hidden" name="UPLOAD_IDENTIFIER" value="<?php echo $unique_upload_id ?>" />
+			<input type="file" name="<?php echo $unique_upload_id ?>" id="<?php echo $unique_upload_id ?>" class='file_upload' /></a>
+		</form><iframe name="u_ifr<?php echo $unique_upload_id ?>" style="display:none;" id="u_ifr<?php echo $unique_upload_id ?>"></iframe>
 	</div>
 
-	<div class="uploading_pane" id="uploading_pane_<?php echo $u_id ?>" style='display:none;'>
+	<div class="uploading_pane" id="uploading_pane_<?php echo $unique_upload_id ?>" style='display:none;'>
 		<div class='bar_summary'><span></span><div class='close'><a href='#'><?php echo utf8_decode('&#215;') ?></a></div></div>
 		<div class='inner_padded'>
 			<div class="form_top">
@@ -15,7 +24,7 @@
 				<div class="uploadBar">
 					<div class="uploadProgOuter"><div class="uploadProgInner">&nbsp;</div></div>
 					<span class="percent_complete">0%</span><a href="#" class="cancel">Cancel</a><div class="clearer"></div>
-					<div class="message" id="upload_status_<?php echo $u_id ?>"><span>Connecting to server...</span></div>
+					<div class="message" id="upload_status_<?php echo $unique_upload_id ?>"><span>Connecting to server...</span></div>
 				</div>
 				<div class='remove'><a href='#'><?php echo utf8_decode('&#215;') ?></a></div>
 			<div class='clearer'></div></div>
